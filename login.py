a1f3599c9276d0fcfe91a8596cdd4ae5f04d6e97
smurzzz <faldashenry25@gmail.com>
2024-10-12 12:42:02 +0800

    first push

---
+++ login.py
@@ -0,0 +1,8 @@
+import eel
+
+
+# Initialize the folder where index.html is located
+eel.init("webb")
+
+# Start the web app and open index.html
+eel.start("admin.html")
---
+++ main.py
@@ -0,0 +1,9 @@
+import eel
+
+
+
+# Initialize the folder where index.html is located
+eel.init("webb")
+
+# Start the web app and open index.html
+eel.start("index.html")
